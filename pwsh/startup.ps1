61c17090075b92afdde76ff67fc9840b47028eee
Bogdan Dodoiu <bdodoiu@gmail.com>
2025-01-19 23:26:17 +0200

    autodetect kubernetes config in .kube/merge/
    enable kubectl autocompletion

--- pwsh/startup.ps1
+++ pwsh/startup.ps1
@@ -12,5 +12,8 @@ function kude([string]$describe)
     kubectl describe $describe
 }
 
+$Env:KUBECONFIG="$Env:KUBECONFIG;$(Get-ChildItem -Path $HOME/.kube/merge | join-string -separator ';')"
+kubectl completion powershell | Out-String | Invoke-Expression
+
 #oh-my-posh init pwsh | Invoke-Expression
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/mojada.omp.json' | Invoke-Expression
